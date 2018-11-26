3e91268e7ece0fcf3ad2d75d4f8ee2b55463ad66
104062108 <davidw70901@gmail.com>
2018-11-26 18:36:52 +0800

    init

---
+++ Assets/Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float m_speed = 5f;
+    public Vector3 teleportPoint;
+    public Rigidbody rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedY = Input.GetAxis("Horizontal");
+        float speedX = Input.GetAxis("Vertical");
+        Vector3 moveVec = new Vector3(speedY, 0, speedX);
+        rb.MovePosition(this.transform.position + Time.deltaTime * m_speed * moveVec);
+    }
+}
---
+++ Assets/Scripts/DoorDetect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorDetect : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("test");
+		anim.SetBool ("character_nearby", true);
+	}
+	void OnTriggerExit(Collider other)
+	{
+		anim.SetBool ("character_nearby", false);
+	}
+	
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+}
