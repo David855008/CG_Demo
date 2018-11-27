af68b4e3852fe9d57a6e6e1d18a9b9b66ed1577d
104062108 <davidw70901@gmail.com>
2018-11-27 18:27:58 +0800

    1127

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -7,6 +7,7 @@ public class Move : MonoBehaviour
     public float m_speed = 5f;
     public Vector3 teleportPoint;
     public Rigidbody rb;
+    public float rotateSpeed;
     // Use this for initialization
     void Start()
     {
@@ -16,9 +17,15 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 rotVec = new Vector3(0, Input.GetAxis("Mouse X"), 0);
+        Vector3 prevRotVec = this.transform.rotation.eulerAngles;
+        Quaternion quaternion = Quaternion.Euler(prevRotVec + Time.deltaTime * rotateSpeed * rotVec);
+        rb.MoveRotation(quaternion);
+
         float speedY = Input.GetAxis("Horizontal");
         float speedX = Input.GetAxis("Vertical");
         Vector3 moveVec = new Vector3(speedY, 0, speedX);
-        rb.MovePosition(this.transform.position + Time.deltaTime * m_speed * moveVec);
+        rb.MovePosition(this.transform.position + quaternion * (Time.deltaTime * m_speed * moveVec));
+
     }
 }
